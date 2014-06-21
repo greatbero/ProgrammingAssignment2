458b149ab83f44dd4e00ddbe1269b7681d864c1e
greatbero <berislav.jozic@gmail.com>
2014-06-21 21:27:15 +0200

    FInal sumbission
    All descriptions entered

--- cachematrix.R
+++ cachematrix.R
@@ -1,48 +1,50 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Programming Assignment 2
 
-## Write a short comment describing this function
+
+## makeCacheMatrix function creates a special "matrix" object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
     m <- NULL
-    set <- function(y) {
-        x <<- y
-        m <<- NULL
+    set <- function(y) {      ## a function setting the variables in the environment
+        x <<- y               ## making "y" the matrix you supplied and 
+        m <<- NULL            ## emtying "m" 
     }
-    get <- function() x
-    setinv <- function(solve) m <<- solve
-    getinv <- function() m
-    list(set = set, get = get,
+    get <- function() x                    ## "get" means "show me x"
+    setinv <- function(solve) m <<- solve  ## "setinv" will store the inversion result ("solve" being the command for inverting the matrix)
+    getinv <- function() m                 ## "getinv" will show the inverted matrix (if it is calculated)
+    list(set = set, get = get,             ##list of variables that can be used as commands in the form "x$variable"
          setinv = setinv,
          getinv = getinv)
 }
 
 
-## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
-    m <- x$getinv()
-    if(!is.null(m)) {
+    m <- x$getinv()                     ## make "m" the value of the list object "getinv" from the list created in makeCacheMatrix 
+    if(!is.null(m)) {                   ## if m is not empty, show it's value 
         message("getting cached data")
         return(m)
     }
-    data <- x$get()
-    m <- solve(data, ...)
-    x$setinv(m)
-    m     ## Return a matrix that is the inverse of 'x'
+    data <- x$get()                     ## fill "data" with value from list object "get" from the list created in makeCacheMatrix
+    m <- solve(data, ...)               ## as m is not yet calculated, calculate it (inversion of matrix)
+    x$setinv(m)                         ## store it for future collection from cache
+    m                                   ## Return result (this time not from cache)
 }
 
-#l <- makeCacheMatrix()   # create cacheable matrix object
-#testmatri <- rbind(c(1, -1/4), c(-1/4, 1)) #create a matrix for testing
-#l$set(testmatri) #assign it to crrated object
-#l$get(testmatri) #check its value
-#cacheSolve(l) #create inverted matrix
-#cacheSolve(l) #create again - should be getting it from cache
-#cacheSolve(l)%*%testmatri #test that matrix is nverted - product should be an identitty matrix
+
+## and here is the set of commands you can use to test hte functions, just removew the "#" from the start of the line:
+
+#l <- makeCacheMatrix()                      ## create cacheable matrix object
+#testmatri <- rbind(c(1, -1/4), c(-1/4, 1))  ## create a matrix for testing
+#l$set(testmatri)                            ## assign it to a created object "l"
+#l$get()                                     ## check its value so you know it's stored
+#cacheSolve(l)                               ## invert the matrix
+#cacheSolve(l)                               ## invert it again - should be getting it from cache this time
+#cacheSolve(l)%*%testmatri                   ## test that matrix is nverted - product should be an identitty matrix
 
 
-# see benefit of caching on a bigger matrix
-#l$set( matrix( rnorm( 1000000 ), 1000, 1000 ) )
-#cacheSolve(l)
-#cacheSolve(l) # much faster
+## see the benefit of caching on a bigger matrix
+
+#l$set(matrix(rnorm(1000000), 1000, 1000))         ## create a bigger matrix
+#cacheSolve(l)                                     ## invert it first time (notice the amount of time it takes)
+#cacheSolve(l)                                     ## second time will be much faster - specially if you have an old computer
 
