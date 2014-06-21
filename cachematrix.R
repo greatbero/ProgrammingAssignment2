b0c499a4d63b588e6801f81b593e8a45f957659f
greatbero <berislav.jozic@gmail.com>
2014-06-21 15:35:54 +0200

    Update
    Working on the assignment

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,45 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setinv <- function(solve) m <<- solve
+    getinv <- function() m
+    list(set = set, get = get,
+         setinv = setinv,
+         getinv = getinv)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    m <- x$getinv()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setinv(m)
+    m     ## Return a matrix that is the inverse of 'x'
 }
+
+#l <- makeCacheMatrix()   # create cacheable matrix object
+#testmatri <- rbind(c(1, -1/4), c(-1/4, 1)) #create a matrix for testing
+#l$set(testmatri) #assign it to crrated object
+#l$get(testmatri) #check its value
+#cacheSolve(l) #create inverted matrix
+#cacheSolve(l) #create again - should be getting it from cache
+#cacheSolve(l)%*%testmatri #test that matrix is nverted - product should be an identitty matrix
+
+
+# see benefit of caching on a bigger matrix
+#l$set( matrix( rnorm( 1000000 ), 1000, 1000 ) )
+#cacheSolve(l)
+#cacheSolve(l) # much faster
+
